e41539876b8d51a65514a986409936b8be7944b6
LWG13 <164619164+LWG13@users.noreply.github.com>
2024-11-01 18:51:15 +0700

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,39 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import Navigation from "./Components/Navigation.jsx"
+import Service from "./Components/Service.jsx"
+import Faq from "./Components/Faq.jsx"
+import { QueryClient, QueryClientProvider} from "react-query"
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 
+const router = createBrowserRouter([
+  {
+    path: "/LWG13-Portfolio/",
+    element: <Navigation />,
+    children: [
+          {
+            path:"/LWG13-Portfolio/",
+            element: <App />,
+          },
+          {
+            path: "/LWG13-Portfolio/service",
+            element: <Service />
+          },
+          {
+            path: "/LWG13-Portfolio/faq",
+            element: <Faq />,
+          }
+    ],
+  },
+]);
+const queryClient = new QueryClient()
 ReactDOM.createRoot(document.getElementById('root')).render(
+ <QueryClientProvider client={queryClient}>
 	<React.StrictMode>
-		<App />
+		<RouterProvider router={router} />
 	</React.StrictMode>
-)
+ </QueryClientProvider>
